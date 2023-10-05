e502541178fa586920da566cab324c6801488dd5
Nathan Cardoso <100364030+Nathan-cardoso@users.noreply.github.com>
2023-10-05 14:35:00 -0300

    Update banco-bom.sql

--- banco-bom.sql
+++ banco-bom.sql
@@ -2,7 +2,7 @@ CREATE TABLE candidato(
 	numero_eleicao varchar(5),
 	nome varchar(60) not null,
 	cpf varchar(11) not null,
-	email varchar() not null,
+	email varchar(70) not null,
 	total_votos int,
 	primary key (numero_eleicao)
 );
