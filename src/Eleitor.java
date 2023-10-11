27c60c1b672b2d721d1ad60f11adb094932324be
Sr-Cardoso <nathancar.linhares77@gmail.com>
2023-10-11 18:30:17 -0300

    feat: add método de cadastro de eleitor e alteração no construtor

--- src/Eleitor.java
+++ src/Eleitor.java
@@ -1,11 +1,15 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class Eleitor extends Pessoa{
     private String matricula;
     private boolean statusDeVoto;
     private String senha;
     
-    public Eleitor(String nome, String email, String cpf, String matricula) {
-        super(nome, email, cpf);
+    public Eleitor(String nome, String cpf, String email, String matricula, String senha) {
+        super(nome, cpf, email);
         this.matricula = matricula;
+        this.senha = senha;
     }
 
     public String getSenha() {
@@ -25,5 +29,39 @@ public class Eleitor extends Pessoa{
         this.senha = senha;
     }
     
+       public void cadastrar(){
+        try {
+            Connection connection = new Conexao().getConnection();
+            String query = "INSERT INTO eleitor (matricula, nome, cpf, email, senha) VALUES (?,?,?,?,?)";
+            PreparedStatement pst = connection.prepareStatement(query);
+
+            pst.setString(1, getMatricula());
+            pst.setString(2, getNome());
+            pst.setString(3, getCpf());
+            pst.setString(4, getEmail());
+            pst.setString(5, getSenha());
+
+            int validacaoCadastro = pst.executeUpdate();
+
+            if(validacaoCadastro > 0){
+
+                System.out.printf("Eleitor %s cadastrado com sucesso!\n", getNome());
+
+            }else{
+
+                System.out.println("Erro no cadastro do eleitor");
+
+            }
+
+            pst.close();
+            connection.close();
+
+
+        } catch (java.sql.SQLException e) {
+
+            System.out.println("Erro de conexão: " + e.getMessage());
+        }
+
+    }
     
 }
