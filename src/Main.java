dbdbed40c537aaac28a155d54c460e17794225e0
Sr-Cardoso <nathancar.linhares77@gmail.com>
2023-10-17 19:49:01 -0300

    feat: adicionando na main a área de eleitores

--- src/Main.java
+++ src/Main.java
@@ -396,7 +396,196 @@ public class Main {
             break; //Direcionamento para a área de adm
 
             case 2:
-            System.out.println("Ok");
+            input.nextLine();
+
+            int navAreaEleitor;
+            do{
+            Menu.loginEleitor();
+            navAreaEleitor = input.nextInt();
+
+            switch(navAreaEleitor){
+                case 1:
+
+                input.nextLine();
+
+                String emailLogin;
+                String senhaLogin;
+
+                System.out.printf("Digite seu email: ");
+                emailLogin = input.nextLine();
+
+                System.out.printf("Digite sua senha: ");
+                senhaLogin = input.nextLine();
+
+                Eleitor user = Eleitor.login(emailLogin, senhaLogin);
+
+                if(user == null){
+
+                    System.out.println("email ou senha invalido!");
+
+                }else{
+
+                    int navLogado;
+
+                    System.out.println("\nOlá, " + user.getNome());
+                    do{
+
+                        Menu.areaEleitor();
+                        navLogado = input.nextInt();
+
+                        switch(navLogado){
+                            case 1:
+
+                            break; // Fim do case 1 da área após o login
+
+                            case 2:
+                            
+                            System.out.println("Seus dados...");
+                            System.out.println("----------------------------------");
+                            System.out.println(user);
+                            System.out.println("----------------------------------");
+
+                            break;// Fim do case 2 da área após o login
+
+                            case 3:
+                            int codParaEdit;
+                            String alteracaoEleitor;
+
+                            Menu.edicaoEleitor();
+                                codParaEdit = input.nextInt();
+
+                                switch(codParaEdit){
+                                    case 1: 
+
+                                    input.nextLine();
+
+                                    System.out.println("Informe o nome: ");
+                                    alteracaoEleitor = input.nextLine();
+
+                                    user.editar(codParaEdit, alteracaoEleitor);
+
+                                    break; // Fim case 1 de edição eleitor
+
+                                    case 2: 
+
+                                    input.nextLine();
+                                    System.out.println("Informe o CPF: ");
+                                    alteracaoEleitor = input.nextLine();
+
+                                    if(!alteracaoEleitor.matches("\\d{11}")){
+
+                                    System.out.println("---------------------------------------------------------------------");
+                                    System.out.println("\tCPF invalido! Foi passado " + alteracaoEleitor.length() + " digitos, era esperado 11");
+                                    System.out.println("---------------------------------------------------------------------");
+
+                                        break;
+                                    }
+
+                                    user.editar(codParaEdit, alteracaoEleitor);
+
+                                    break;
+
+                                    case 3: 
+
+                                    input.nextLine();
+                                    System.out.println("Informe o novo E-mail: ");
+                                    alteracaoEleitor = input.nextLine();
+
+                                    user.editar(codParaEdit, alteracaoEleitor);
+
+                                    break; // Fim case 3 de edição eleitor
+
+                                    case 4: 
+
+                                    input.nextLine();
+                                    System.out.println("Informe a nova senha: ");
+                                    alteracaoEleitor = input.nextLine();
+
+                                    user.editar(codParaEdit, alteracaoEleitor);
+
+                                    break; // Fim case 4 de edição eleitor
+
+                                    case 5: 
+                                    break; // Fim case 5 de edição eleitor
+
+                                    default:
+                                        System.out.println("Erro de digitação");
+
+                                    break;
+
+                                }
+
+                            break;// Fim do case 3 da área após o login
+
+                            case 4:
+                            break;// Fim do case 4 da área após o login
+
+                            default:
+                            System.out.println("Erro de digitação");
+                            break;
+                        }
+
+                    }while(navLogado != 4);
+                    
+
+                }
+
+
+                break; //Breack do case 1 da área de eleitores
+
+                case 2:
+                        String matricula;
+                        String nome;
+                        String cpf;
+                        String email;
+                        String senha;
+
+                        input.nextLine();
+                        System.out.println("Digite a matrícula do eleitor: ");
+                        matricula = input.nextLine();
+
+                        if(!matricula.matches("\\d{10}")){
+                            System.out.println("------------------------------------------------");
+                            System.out.println("    Erro ao cadastrar o número do eleitor.\n\tEra esperado 10 digitos numericos");
+                            System.out.println("------------------------------------------------");
+                            break;
+                        }
+
+                        System.out.println("Informe o nome do eleitor: ");
+                        nome = input.nextLine();
+
+                        System.out.println("Informe o cpf do eleitor: ");
+                        cpf = input.nextLine();
+
+                        if(!cpf.matches("\\d{11}")){
+                            System.out.println("---------------------------------------------------------------------");
+                            System.out.println("\tCPF invalido! Foi passado " + cpf.length() + " digitos, era esperado 11");
+                            System.out.println("---------------------------------------------------------------------");
+                            break;
+                        }
+
+                        System.out.println("Digite o emeil do eleitor: ");
+                        email = input.nextLine();
+
+                        System.out.println("Digite sua senha: ");
+                        senha = input.nextLine();
+
+                        Eleitor eleitor = new Eleitor(nome, cpf, email, matricula,senha);
+
+                        eleitor.cadastrar();
+
+                break; //Break do case 2 da área de eleitores
+
+                case 3:
+                break;
+
+                default:
+                System.out.println("Erro de digitação");
+                break;
+
+            }
+            }while(navAreaEleitor != 3);
+
             break;
 
             case 3:
